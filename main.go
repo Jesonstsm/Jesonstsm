6de73ed88d82611db63933a2e24c8f0bc60354e6
Jesonstsm <66990546+Jesonstsm@users.noreply.github.com>
2020-06-15 23:31:50 -0700

    good
    good

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"golang.org/x/net/websocket"
+	"log"
+	"net/http"
+)
+
+func main()  {
+	var serverAddr string
+	flag.StringVar(&serverAddr, "l", "0.0.0.0:80", "listen addr")
+	flag.Parse()
+
+	log.Println("start")
+	http.Handle("/hello", websocket.Handler(WebsocketConn))
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		//writer.Header().Set("Location", "https://google.com")
+		writer.Write([]byte("Test OK"))
+	})
+
+	err := http.ListenAndServe(serverAddr,  nil)
+	if err != nil {
+		log.Println("Listen Error:", err.Error())
+	}
+}
+
+func WebsocketConn(ws *websocket.Conn) {
+	ws.Write([]byte("TestOk"))
+	ws.Close()
+}
